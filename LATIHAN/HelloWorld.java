f2acf6524ceb48a40ed17b3045b12f2cae7b01ce
Amar Dewa <opiexbmz@student.telkomuniversity.ac.id>
2020-09-17 14:41:44 +0700

    HelloWorld.java

---
+++ LATIHAN/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+
+	public static void main (String[] args) {
+		System.out.println("Nama 	: Amar Dewa"); 
+        System.out.println("NIM	: 6706204007");
+        System.out.println("Kelas 	: D3IF 44-03");
+	}
+}
+	
